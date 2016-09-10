02f5eb376e14351d253ad27a3616f5ec9c2b3a4f
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-10 01:38:38 -0400

    cleaned up formulas

--- src/gcLabs/Lab4Factorial.java
+++ src/gcLabs/Lab4Factorial.java
@@ -19,19 +19,35 @@ public class Lab4Factorial {
 		// TODO Auto-generated method stub
 		
 		//declare variables
-		int number;
+		int number = 1;
+		int i = 1;
+		int factorial = 1;
+		
 		String choice;
 		choice = "y";
 		
-		//sysout and prompts
+		//sysout and prompts for user input
 		System.out.println("FACTORIAL CALCULATOR");
+		System.out.println();
+		
+	//loop
+	do{
 		System.out.println();
 		System.out.println("Enter a number from 1 to 10: ");
 		Scanner k = new Scanner(System.in);
 		number = k.nextInt();
+		System.out.println();
+		
+		//formula to find factorial
+		for( i = 1; i <=number; i++);
+			factorial = factorial * i;
+		
+		//output and prompt
+		System.out.println("The factorial of " + (number) + " is " + (factorial));	
+		System.out.println();
 		
 		
-		
+	}while (choice.equals("y"));
 	}
 
 }
