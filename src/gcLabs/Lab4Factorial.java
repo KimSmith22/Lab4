53963d30d9c772e06ca74358e40b876e324f944f
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-10 01:11:10 -0400

    user input /output

--- src/gcLabs/Lab4Factorial.java
+++ src/gcLabs/Lab4Factorial.java
@@ -3,6 +3,8 @@
  */
 package gcLabs;
 
+import java.util.Scanner;
+
 /**
  * @author Kimberli
  *
@@ -12,9 +14,24 @@ public class Lab4Factorial {
 	/**
 	 * @param args
 	 */
+	//method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//declare variables
+		int number;
+		String choice;
+		choice = "y";
+		
+		//sysout and prompts
+		System.out.println("FACTORIAL CALCULATOR");
+		System.out.println();
+		System.out.println("Enter a number from 1 to 10: ");
+		Scanner k = new Scanner(System.in);
+		number = k.nextInt();
+		
+		
+		
 	}
 
 }
