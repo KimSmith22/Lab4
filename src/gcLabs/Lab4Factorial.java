f52d41d7a1ad0c79a390231be432329ce8cd2d13
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-10 00:20:51 -0400

    initial commit

---
+++ src/gcLabs/Lab4Factorial.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gcLabs;
+
+/**
+ * @author Kimberli
+ *
+ */
+public class Lab4Factorial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
